c7ccfd2693cbdd914f512edfaa2beb76edd17218
rbellatinf <ricardo.bellatinf@gmail.com>
2026-08-13 16:05:59 -0500

    refactor: remove Spree runtime and legacy CI

--- backend/spree/extensions/app/models/rumbo/associate.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Rumbo
-  class Associate < ApplicationRecord
-    self.table_name = "rumbo_associates"
-
-    validates :spree_customer_id, :referral_code, presence: true, uniqueness: true
-  end
-end
